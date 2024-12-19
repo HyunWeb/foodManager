9cb2b7f641792f81af00d73203bc86b9c6fce4f0
HyunWeb <mattew1803@naver.com>
2024-12-19 10:41:08 +0900

    안쓰는 파일 정리

--- frontend/src/components/molecules/ChartBlock.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ChartBlock() {
-  return <div>ChartBlock</div>;
-}
--- frontend/src/components/molecules/ExpirationInfo.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ExpirationInfo() {
-  return <div>ExpirationInfo</div>;
-}
